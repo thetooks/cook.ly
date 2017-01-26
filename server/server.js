8acc85725c3ccff5a9e585a4b6d255f4bdaefd4b
Benson Ninh <bensonninh@gmail.com>
2017-01-25 19:45:54 -0800

    make server display hello world; comment out lines that will be necessary later down the line to serve up client files

--- server/server.js
+++ server/server.js
@@ -1,12 +1,19 @@
 var express = require('express');
+// var db = require('../db/db.js')
 var app = express();
 
 var port = 3000;
 
+module.exports = app;
+
+
+// app.use(express.static(__dirname + '/../client'));
+
+// Handle route requests
 app.get('/', function(req, res) {
   res.send('hello world');
 });
 
-app.listen(port);
-
-module.exports = app;
+app.listen(port, function() {
+  console.log('Listening on port: ' + port);
+});
