159b5b228206f8aea315c27395c65b1666696f1f
Benson Ninh <bensonninh@gmail.com>
2017-01-26 19:07:29 -0800

    clean up unncessary comments

--- server/server.js
+++ server/server.js
@@ -1,19 +1,13 @@
 var express = require('express');
 var webpack = require('webpack');
 var path = require('path');
-// var config = require('../webpack.config.js');
-// var webpackMiddleware = require('webpack-dev-middleware');
 // var db = require('../db/db.js')
 var app = express();
 var port = 3000;
 
-// var compiler = webpack(config);
-
 module.exports = app;
 
-// uncomment this when the client is ready to be served
 app.use(express.static(path.join(__dirname, '/../client')));
-// app.use(webpackMiddleware(compiler));
 
 app.listen(port, function() {
   console.log('Listening on port: ' + port);
