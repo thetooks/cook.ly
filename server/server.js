4ddd6e6850d5cda56fac0f2d4d0a78242dd61e7a
Benson Ninh <bensonninh@gmail.com>
2017-01-26 18:59:56 -0800

    implement use of static assets handle '/' get request to load homepage

--- server/server.js
+++ server/server.js
@@ -1,19 +1,38 @@
 var express = require('express');
+var webpack = require('webpack');
+var path = require('path');
+// var config = require('../webpack.config.js');
+// var webpackMiddleware = require('webpack-dev-middleware');
 // var db = require('../db/db.js')
 var app = express();
-
 var port = 3000;
+
+// var compiler = webpack(config);
 
 module.exports = app;
 
-
-// app.use(express.static(__dirname + '/../client'));
-
-// Handle route requests
-app.get('/', function(req, res) {
-  res.send('hello world');
-});
+// uncomment this when the client is ready to be served
+app.use(express.static(path.join(__dirname, '/../client')));
+// app.use(webpackMiddleware(compiler));
 
 app.listen(port, function() {
   console.log('Listening on port: ' + port);
 });
+
+// HANDLE ROUTING
+// landing page
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/client/index.html'));
+});
+
+// // user routes
+// app.get('/user', function() {
+//   /* DO SOMETHING */
+//   // render the user page
+//   res.send();
+// })
+
+// app.post('/user', function() {
+//   res.send();
+// })
+// // host routes
