ab5f3e4684962e41c4bc0d699effdff3edc8fe86
Benson Ninh <bensonninh@gmail.com>
2017-01-26 18:57:16 -0800

    change destination for output file to client

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,23 @@
+var path = require('path');
+
+const OUTPUT_PATH = path.join(__dirname, '/client/build');
+
 module.exports = {
   entry: './client/index.jsx',
   
   output: {
-    path: './build',
-    filename: 'bundle.js'
+    path: __dirname + '/client/build',
+    filename: 'bundle.js',
   },
 
   module: {
     loaders: [{
       test: /\.jsx?/,
-      loader: 'babel'
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015']
+      }
     }]
   }
 };
