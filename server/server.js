2787a1265364557e5db1eba7f48cb05137ea25c6
Benson Ninh <bensonninh@gmail.com>
2017-01-29 13:52:13 -0800

    make app use a router for for all request handling remove excess comments

--- server/server.js
+++ server/server.js
@@ -1,32 +1,17 @@
 var express = require('express');
-var webpack = require('webpack');
 var path = require('path');
+var router = require('./routes.js');
 // var db = require('../db/db.js')
 var app = express();
 var port = 3000;
-
-module.exports = app;
-
-app.use(express.static(path.join(__dirname, '/../client')));
 
 app.listen(port, function() {
   console.log('Listening on port: ' + port);
 });
 
+app.use(express.static(path.join(__dirname, '/../client')));
+
 // HANDLE ROUTING
-// landing page
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '/client/index.html'));
-});
+app.use('/', router);
 
-// // user routes
-// app.get('/user', function() {
-//   /* DO SOMETHING */
-//   // render the user page
-//   res.send();
-// })
-
-// app.post('/user', function() {
-//   res.send();
-// })
-// // host routes
+module.exports = app;
