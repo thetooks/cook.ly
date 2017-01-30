b4965012c6c3e2960f2f4ebc61c3be95d5a8d49c
Chike <chike.onuorah@gmail.com>
2017-01-30 12:04:31 -0800

    configure database

---
+++ db/modeltest.js
@@ -0,0 +1,14 @@
+var Models = require('../cooklydatabase.js');
+var User = Models.User;
+var Location = Models.Location;
+var UserPreference = Models.UserPreference;
+var Host = Models.Host;
+var Review = Models.Review;
+var Menu = Models.Menu;
+var Event = Models.Event;
+var UserBooking = Models.UserBooking;
+var Speciality = Models.Specialty;
+
+
+
+console.dir(Models.Location, '?????');
