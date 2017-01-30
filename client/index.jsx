b96bf9f44fcbb1ba32d9719e593e3dac1d441c43
Richard Huber <rwhuber13@gmail.com>
2017-01-29 17:45:38 -0800

    Render react router

--- client/index.jsx
+++ client/index.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router, Route, Link, IndexRoute, browserHistory} from 'react-router';
 import App from './components/App.jsx';
+import Host from './components/HostPage.jsx';
 
-ReactDOM.render(
-  <App/>,
-  document.getElementById('app')
-);
+// ReactDOM.render(
+//   <App/>,
+//   document.getElementById('app')
+// );
+
+ReactDOM.render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+    </Route>
+  </Router>
+), document.getElementById('app'));
