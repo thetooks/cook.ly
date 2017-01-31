00d1c9bb73411e88ad9f6bd7c2e6dad3e3d2f1af
Benson Ninh <bensonninh@gmail.com>
2017-01-31 14:29:35 -0800

    add a userBookings component for the user page

---
+++ client/components/User/userBookings.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class UserBookings extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div>USER BOOKINGS MODULE</div>
+    );
+  }
+};
+
+module.exports = UserBookings;
