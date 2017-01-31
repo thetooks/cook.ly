fd04bf236d7b7328d32c9d177a48ca7887953758
Benson Ninh <bensonninh@gmail.com>
2017-01-30 17:56:44 -0800

    add button to test jquery request to server

--- client/components/Home/App.jsx
+++ client/components/Home/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from './nav.jsx';
 import Carousel from './carousel.jsx';
+import $ from 'jquery';
 
 
 class App extends React.Component {
@@ -9,14 +10,31 @@ class App extends React.Component {
 
     this.state = {
     };
+
+    this.getAllEvents = this.getAllEvents.bind(this);
+  }
+
+  getAllEvents() {
+    $.ajax({
+      url: '/getallevents',
+      method: 'GET',
+      dataType: 'json',
+      success: function(data) {
+        console.log(data);
+      },
+      error: function(err, data) {
+        console.log(err, data);
+        throw err;
+      }
+    })
   }
 
   render() {
     return (
       <div >
         <Navbar />
-        
         {this.props.children}
+        <button onClick={this.getAllEvents}>Get all events</button>
       </div>
     );
   }
