dd28820390eba97e8aa175673019fe4ad963ea83
Richard Huber <rwhuber13@gmail.com>
2017-01-31 20:23:20 -0800

    Add upcomming events list when view calendar is clicked

--- client/components/Host/host.jsx
+++ client/components/Host/host.jsx
@@ -2,21 +2,57 @@ import React from 'react';
 import {Grid, Row, Col, ButtonToolbar, ButtonGroup, Button} from 'react-bootstrap';
 import Calendar from 'rc-calendar';
 import Thumbnail from './thumbnail.jsx';
+import $ from 'jquery';
+
 
 class Host extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       showCalendar: false,
+      events: []
     };
 
+    this.getAllEvents = this.getAllEvents.bind(this);
     this.calendarClick = this.calendarClick.bind(this);
+    this.eventList = this.eventList.bind(this);
+  }
+
+  getAllEvents() {
+    var context = this;
+
+    $.ajax({
+      url: '/api/allUpcommingEvents',
+      method: 'GET',
+      success: function(data) {
+        context.setState({
+          events: data
+        });
+      },
+      error: function(err, data) {
+        console.log(err, data);
+        throw err;
+      }
+    });
+  }
+
+  eventList() {
+    var list = this.state.events.map((item) =>
+      <li>{item.TimeStamp}</li>
+    );
+    return (
+      <div>
+        <h4>Upcoming Events</h4>
+        <ul>{list}</ul>
+      </div>
+    );
   }
 
   calendarClick() {
     this.setState({
       showCalendar: !this.state.showCalendar
     });
+    this.getAllEvents();
   }
 
   render() {
@@ -41,7 +77,10 @@ class Host extends React.Component {
             <Col sm={4}>
               {Thumbnail} 
             </Col>
-            <Col sm={8}>
+            <Col sm={4}>
+              {this.state.showCalendar && this.eventList()}
+            </Col>
+            <Col sm={4}>
               {this.state.showCalendar && <Calendar />}
             </Col>
           </Row>
