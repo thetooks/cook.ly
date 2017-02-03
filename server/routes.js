b26413d5955f8d675366d25b4b0beaf049d8c705
Benson Ninh <bensonninh@gmail.com>
2017-02-02 18:18:02 -0800

    viewing user's bookings now shows the user's events for userId = 1

--- server/routes.js
+++ server/routes.js
@@ -6,41 +6,37 @@ var connection = new Sequelize('cookly', 'root', '');
 var Models = require('../cooklydatabase.js');
 var User = Models.User;
 var Location = Models.Location;
-var UserPreference = Models.UserPreference;
 var Host = Models.Host;
 var Review = Models.Review;
-var Menu = Models.Menu;
 var Event = Models.Event;
 var UserBooking = Models.UserBooking;
-var Specialty = Models.Specialty;
 
 router.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/client/index.html'));
 });
 
-router.get('/getAllEvents', function(req, res){
-  Event.build({
-    TimeStamp: '2017-01-01',
-    Cuisine: 'Rainbow Cake',
-    MaxsSeats: 20,
-    Address: 'Cake Street, Rainbow Unicorn Island'
-  })
-  .save()
-  .then(function() {
-    res.send('Event added!');
-  });
-
-  // query the data
-  // push the data into events as they come in
-});
-
 router.get('/getUserEvents', function(req, res) {
   // USER TABLE IS CURRENTLY BROKEN
   // PULL STRAIGHT FROM EVENTS TABLE REGARDLESS OF USER INVOLVEMENT OF THE EVENT
-  Event.findAll()
-    .then(function(data) {
-      res.send(JSON.stringify(data));
+
+  UserBooking.findAll({
+    where: {
+      UserId: 1
+    }
+  }).then(function(events) {
+    var data = JSON.stringify(events);
+    var parsedData = JSON.parse(data);
+    var allIds = parsedData.map(function(event) {
+      return {id: event.EventId};
     });
+    Event.findAll({
+      where: {
+        $or: allIds
+      }
+    }).then(function(events) {
+      res.send(JSON.stringify(events));
+    });
+  });
 });
 
 router.get('/getEvents', function(req, res) {
