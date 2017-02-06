a7d9d6adfd5cf25269b560487b1a515c248fffb1
Richard Huber <rwhuber13@gmail.com>
2017-02-06 15:44:14 -0800

    Change carousel wording

--- client/components/Home/carousel.jsx
+++ client/components/Home/carousel.jsx
@@ -9,22 +9,22 @@ class carouselInstance extends React.Component {
         <Carousel.Item>
           <img width={900} height={500} alt="900x500" src="./assets/diningTable2.jpg"/>
           <Carousel.Caption>
-            <h1>welcome To Cook.ly</h1>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            <h1>Welcome to Cook.ly</h1>
+            <h4>Connecting communities through food.</h4>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img width={900} height={500} alt="900x500" src="./assets/diningTable1.jpeg"/>
           <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <h1>As a Host</h1>
+            <h4>Share your cooking talent with the world!</h4>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img width={900} height={500} alt="900x500" src="./assets/diningTable.jpeg"/>
           <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+            <h1>As a Guest</h1>
+            <h4>Enjoy a unique dining experience with friends</h4>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
