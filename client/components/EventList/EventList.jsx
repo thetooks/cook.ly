23eb662a61262f932951d45f94421babb58c5d6c
ntoung <nmtoung@gmail.com>
2017-02-08 10:17:59 -0800

    Add skeleton files for Search & EventList components

---
+++ client/components/EventList/EventList.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class EventList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (<div>
+      Hello
+    </div>);
+  }
+}
+
+export default EventList;
---
+++ client/components/Search/Search.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+var Search = (props) => {
+  // constructor(props) {
+  //   super(props);
+  // }
+  // return 
+  // render() {
+    return (
+      <div>
+        Hello
+      </div>
+    )
+  // }
+}
+
+export default Search;
